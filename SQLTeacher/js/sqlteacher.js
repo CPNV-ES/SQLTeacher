b32e3742562aa2f40489502ad645fb129110690b
BenjaminDelacombaz <benjamin.delacombaz@cpnv.ch>
2019-03-25 16:59:16 +0100

    Try to fix exam check value

--- SQLTeacher/js/sqlteacher.js
+++ SQLTeacher/js/sqlteacher.js
@@ -1,4 +1,9 @@
-﻿$('.isActiveCheckBox').change(sender => {
+﻿var pinCodeValid = false
+var buttonsValid = $('.submitQuery').toArray().map(button => {
+    return { button: button, queryValid: false }
+})
+
+$('.isActiveCheckBox').change(sender => {
     // Get the current value
     let currentValue = sender.currentTarget.checked
     // Get checkboxes list
@@ -39,6 +44,28 @@ $('.submitQuery').click(sender => {
     const query = sender.currentTarget.parentElement.parentElement.getElementsByClassName('queryResponse')[0].value
     // Get the id of the sender
     const currentId = sender.currentTarget.parentElement.parentElement.getElementsByClassName('colId')[0].innerText
+    // Get the user pin code
+    const pinCode = $('#pinCode')[0].value
 
+})
 
+$('#pinCode').keyup(sender => {
+    const currentPinCode = sender.currentTarget.value
+    pinCodeValid = (currentPinCode.length == 4)
+
+    for (const button of buttonsValid) {
+        button.disabled = !(pinCodeValid && button.queryValid)
+    }
+})
+
+$('.queryResponse').keyup(sender => {
+    const button = sender.currentTarget.parentElement.parentElement.getElementsByClassName('submitQuery')[0]
+
+    for (const buttonListed of buttonsValid) {
+        if (buttonListed.button == button) {
+            buttonListed.queryValid = (sender.currentTarget.value != "")
+        }
+    }
+
+    button.disabled = !(sender.currentTarget.value != "" && pinCodeValid)
 })
