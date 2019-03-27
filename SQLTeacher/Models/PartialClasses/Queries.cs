d1984f9c54cef4f101a50afe281679db2af42e11
BenjaminDelacombaz <benjamin.delacombaz@cpnv.ch>
2019-03-27 07:36:57 +0100

    Try to test to array result

--- SQLTeacher/Models/PartialClasses/Queries.cs
+++ SQLTeacher/Models/PartialClasses/Queries.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data.SqlClient;
+using System.Linq;
 
 namespace SQLTeacher.Models
 {
@@ -12,13 +13,15 @@ namespace SQLTeacher.Models
     {
         public Boolean checkStatement(string query)
         {
-            ArrayList al = new ArrayList();
+            ArrayList dataFromStudent = new ArrayList();
+            ArrayList dataCorrect = new ArrayList();
 
             SqlConnection conn = new SqlConnection("Server=localhost;Database=TestExercice1;Trusted_Connection=True;");
             SqlCommand cmd = new SqlCommand();
             SqlDataReader dr;
             try
             {
+                // Get student result
                 cmd.CommandText = query;
                 cmd.Connection = conn;
                 cmd.Connection.Open();
@@ -33,14 +36,42 @@ namespace SQLTeacher.Models
                     dr.GetValues(numb);
 
                     // Add this Row to ArrayList.
-                    al.Add(numb);
+                    dataFromStudent.Add(numb);
                 }
+                dr.Close();
+
+                // Get correct value
+                cmd.CommandText = this.Statement;
+
+                dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    Object[] numb = new Object[dr.FieldCount];
+
+                    // Get the Row with all its column values..
+                    dr.GetValues(numb);
+
+                    // Add this Row to ArrayList.
+                    dataCorrect.Add(numb);
+                }
+                dr.Close();
             }
             finally
             {
                 conn.Close();
                 conn.Dispose();
             }
+
+            for (int i = 0; i < dataFromStudent.Count; i++)
+            {
+                if (dataFromStudent[i].Equals(dataCorrect[i]))
+                {
+                    var tutu = false;
+                    break;
+                }
+            }
+
             return (query == this.Statement);
         }
     }
